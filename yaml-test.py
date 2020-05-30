1b48c602014b10ceae48e34dbea779c6a56da1bb
John Alcock <john.alcock@bns.co.uk>
2020-05-30 21:04:49 +0100

    Added test file for yaml and config.yml

---
+++ yaml-test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+from time import sleep
+import yaml
+config = yaml.safe_load(open("config.yml"))
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+TDS_Relay = config.TDS_Relay
+GPIO.setup(TDS_Relay, GPIO.OUT)
+GPIO.output(TDS_Relay, GPIO.HIGH)
+sleep(2)
+GPIO.output(TDS_Relay, GPIO.LOW)
+sleep(2)
+GPIO.output(TDS_Relay, GPIO.HIGH)
