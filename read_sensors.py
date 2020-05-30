6abb52b3daef160042e94d0892bb94314bd3d14b
John Alcock <john.alcock@bns.co.uk>
2020-05-30 19:42:42 +0100

    Reversed tds relay output

--- read_sensors.py
+++ read_sensors.py
@@ -12,7 +12,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 TDS_Relay = 21
 GPIO.setup(TDS_Relay, GPIO.OUT)
-GPIO.output(TDS_Relay, GPIO.LOW)
+GPIO.output(TDS_Relay, GPIO.HIGH)
 import datetime
 import firebase_admin
 from firebase_admin import credentials
@@ -30,12 +30,12 @@ os.system('modprobe w1-therm')
 water_temp_sensor = '/sys/bus/w1/devices/28-01192a88d530/w1_slave'
 
 def read_water_temp_raw():
-    GPIO.output(TDS_Relay, GPIO.HIGH)
+    GPIO.output(TDS_Relay, GPIO.LOW)
     sleep(1)
     f = open(water_temp_sensor, 'r')
     lines = f.readlines()
     f.close()
-    GPIO.output(TDS_Relay, GPIO.LOW)
+    GPIO.output(TDS_Relay, GPIO.HIGH)
     sleep(1)
     return lines
 
