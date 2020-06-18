db09f94790d3be84d0952ce0db26c6b6e0eae537
John Alcock <john.alcock@bns.co.uk>
2020-06-18 20:18:56 +0100

    Yaml config added, created sample config and changed TDS reading to use an average of 8 readings

--- read_sensors.py
+++ read_sensors.py
@@ -2,6 +2,9 @@
 
 import os
 from time import sleep
+import yaml
+config = yaml.safe_load(open("config.yml"))
+
 import Adafruit_DHT
 import board
 import busio
@@ -10,7 +13,7 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-TDS_Relay = 21
+TDS_Relay = config["tds-relay"]
 GPIO.setup(TDS_Relay, GPIO.OUT)
 GPIO.output(TDS_Relay, GPIO.HIGH)
 import datetime
@@ -18,25 +21,21 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('./firebase-certificate.json')
+cred = credentials.Certificate(config["firebase-certificate"])
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# Water temperature stuff
+# Water temperature setup
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-water_temp_sensor = '/sys/bus/w1/devices/28-01192a88d530/w1_slave'
+water_temp_sensor = config["water-temperature-device"]
 
 def read_water_temp_raw():
-    GPIO.output(TDS_Relay, GPIO.LOW)
-    sleep(5)
     f = open(water_temp_sensor, 'r')
     lines = f.readlines()
     f.close()
-    GPIO.output(TDS_Relay, GPIO.HIGH)
-    sleep(2)
     return lines
 
 def read_water_temp():
@@ -58,7 +57,7 @@ sleep(1)
 
 # Ambient stuff here
 DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 19
+DHT_PIN = config["ambient-sensor"]
 humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 if humidity is not None and temperature is not None:
     print("Room Temperature={0:0.2f}*C  Humidity={1:0.2f}%".format(temperature, humidity))
@@ -70,12 +69,21 @@ ads = ADS.ADS1115(i2c)
 
 #pH
 ph = AnalogIn(ads, ADS.P0)
-pHValue=(-5.7*ph.voltage+21.34)
+pHValue=(config["ph"]["m"]*ph.voltage+config["ph"]["b"])
 print('pH: '+str(pHValue))
 
 #TDS
 tds = AnalogIn(ads, ADS.P1)
-tdsValue=(133.42*tds.voltage*tds.voltage*tds.voltage - 255.86*tds.voltage*tds.voltage + 857.39*tds.voltage)*0.5
+def read_tds():
+    tdsValues = []
+    GPIO.output(TDS_Relay, GPIO.LOW)
+    sleep(5)
+    for x in range(7):
+        tdsValues.append((133.42*tds.voltage*tds.voltage*tds.voltage - 255.86*tds.voltage*tds.voltage + 857.39*tds.voltage)*0.5)
+        wait(1)
+    tdsValues.sort()
+    return mean(tdsValues[1,-1])
+tdsValue = read_tds()
 print('TDS (ppm): ' + str(tdsValue))
 
 #Store it on firestore
